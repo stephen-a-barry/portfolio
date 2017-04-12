7ee00da485adc49398fb73197e24fb1858bb03c4
stephen-andrew-barry <stephen.andrew.barry@gmail.com>
2017-04-12 11:03:04 -0700

    some styling changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // smooth scroll
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
-      if( $(this).attr("class")=="smooth-scroll") {
+      if( $(this).attr("class")=="smooth-scroll" || $(this).attr("id")=="hometown") {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
